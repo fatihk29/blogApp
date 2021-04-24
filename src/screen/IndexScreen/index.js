887b42f66f4fb795929b51010b54b01f0943b1d1
fatihk29 <fatihkrc7@gmail.com>
2021-04-24 19:24:50 +0300

    context updated

--- src/screen/IndexScreen/index.js
+++ src/screen/IndexScreen/index.js
@@ -1,23 +1,53 @@
 import React, {useContext} from 'react';
-import {View, Text, FlatList, Button} from 'react-native';
-import BlogContext from '../../context/BlogContext';
+import {
+  View,
+  Text,
+  FlatList,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import {Context} from '../../context/BlogContext';
 
 const IndexScreen = () => {
-  const {data, addBlogPost} = useContext(BlogContext);
+  const {state, addBlogPost} = useContext(Context);
 
   return (
     <View>
       <Text>Index Screeen</Text>
       <Button title="Add Post" onPress={() => addBlogPost()} />
       <FlatList
-        data={data}
+        data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({item}) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>
+                {item.title}- Nu:{item.id}
+              </Text>
+              <TouchableOpacity onPress={() => console.log(item.id)}>
+                <Text>Trash</Text>
+              </TouchableOpacity>
+            </View>
+          );
         }}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    borderTopWidth: 1,
+    borderColor: 'gray',
+  },
+  title: {
+    fontSize: 20,
+  },
+});
 
 export default IndexScreen;
